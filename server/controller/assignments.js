8f5272729bebe56b878f81e6aab1748489eb2b4a
AbishanJeyanathan <74685751+AbishanJeyanathan@users.noreply.github.com>
2023-12-07 19:52:24 -0500

    commit

--- server/controller/assignments.js
+++ server/controller/assignments.js
@@ -1,15 +1,18 @@
+// Import necessary modules and dependiencies
 let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 
-
+// Import the Assignment model
 let Assignment = require('../models/assignments');
 
 
-
+// Display the list of assignments
 module.exports.displayassignmentlist = async(req, res, next) => { //use async for updated mongoose
     try {
+        // Retrieve all assignments from the database
         const reportList = await Assignment.find();
+        // Render the list view with assignment data
         res.render('assignment/list', {
             title: 'report',
             report: reportList,
@@ -17,22 +20,25 @@ module.exports.displayassignmentlist = async(req, res, next) => { //use async fo
         });
     } catch (err) {
         console.error(err);
-        // Handls error
+        // Handle errors while retching data
         res.render('assignment/list', {
             error: 'Error on server'
         });
     }
 }
 
-
+// Render the page to add a new assignment
 module.exports.displayAddPage = async(req, res, next) => {
         res.render('assignment/add', {
             title: 'Add Assignment',
             displayName: req.user ? req.user.displayName : ''
         });
-    } //renders the add page
+    } // renders the add page
+
+// Process addition of a new assignment
 module.exports.processAddPage = async(req, res, next) => {
     try {
+        // Create a new assignment using data from the request body
         const newAssignment = await Assignment.create({ //uses await because of updated mongoose
             "Name": req.body.Name,
             "DateTime": req.body.DateTime,
@@ -42,22 +48,24 @@ module.exports.processAddPage = async(req, res, next) => {
             "Damage": req.body.Damage,
             "Status": req.body.Status
         });
-
+        // Redirect to the assignments page after addition
         res.redirect('/assignments'); //redirects to the assignment page after adding new item
     } catch (err) {
-        //handles the error
+        //handles the error during the assignment reaction
         console.error(err);
         res.render('assignment/add', { error: 'There is an error on server' });
     }
 }
 
 
-
+// Display the page to edit an assignment
 module.exports.displayEditPage = async(req, res, next) => {
     let id = req.params.id;
 
     try {
+        // Find the assignment by ID for editing
         const assignmentToEdit = await Assignment.findById(id);
+        // Render the edit page with assignment data
         res.render('assignment/edit', {
             title: 'Edit Assignment',
             assignments: assignmentToEdit,
@@ -68,6 +76,7 @@ module.exports.displayEditPage = async(req, res, next) => {
         res.render('assignment/edit', { error: 'there is an error on server' });
     }
 }
+// Precess the editing of an assignment
 module.exports.processEditPage = async(req, res, next) => {
     let id = req.params.id;
 
@@ -81,8 +90,9 @@ module.exports.processEditPage = async(req, res, next) => {
             "Damage": req.body.Damage,
             "Status": req.body.Status
         };
-
+        // Update the assignment using its ID
         await Assignment.updateOne({ _id: id }, updatedAssignments); //updates using id
+        // Redirect to assignments page after update
         res.redirect('/assignments'); //redirects to the assignment page after adding new item
     } catch (err) {
         console.error(err);
@@ -90,11 +100,14 @@ module.exports.processEditPage = async(req, res, next) => {
     }
 }
 
+// Perform the deletion of an assignment
 module.exports.preformDelete = async(req, res, next) => {
     let id = req.params.id;
 
     try {
-        await Assignment.deleteOne({ _id: id }); //does the delete
+        // Delete an assignment by its ID
+        await Assignment.deleteOne({ _id: id });
+        // Redirect to assignment page after the deletion
         res.redirect('/assignments'); //redirects to the assignment page after adding new item
     } catch (err) {
         console.error(err);
