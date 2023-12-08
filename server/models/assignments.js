046ba67444233f12d7a56cf65905e90d718794d7
AbishanJeyanathan <74685751+AbishanJeyanathan@users.noreply.github.com>
2023-12-07 19:56:35 -0500

    abishan

--- server/models/assignments.js
+++ server/models/assignments.js
@@ -1,5 +1,7 @@
+// Import the Mongoose library for MongoDB Interactions
 let mongoose = require('mongoose');
 
+// Define a Mongoose schema for the 'report' collection
 let reportModel = mongoose.Schema({
     Name: String,
     DateTime: String,
@@ -12,4 +14,5 @@ let reportModel = mongoose.Schema({
     collection: "report"
 });
 
+// Create and export a Mongoose model based on the reportModel schema
 module.exports = mongoose.model('reports', reportModel);
